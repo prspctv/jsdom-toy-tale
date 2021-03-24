073caa1fd5c865326f0dc47c914a734c847ad863
William Zembricki <wzembricki@gmail.com>
2021-03-24 11:21:37 -0400

    driver change

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 let addToy = false;
 
-document.addEventListener("DOMContentLoaded", () => {
+  const toyCollection = document.querySelector("#toy-collection")
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
@@ -11,5 +11,43 @@ document.addEventListener("DOMContentLoaded", () => {
     } else {
       toyFormContainer.style.display = "none";
     }
+
+
   });
-});
+fetch("http://localhost:3000/toys")
+  .then(res => res.json())
+  .then(function(toyArray){
+    toyArray.forEach(function(toyObj){
+      turnToyToDiv(toyObj)
+      console.log("fetch block happened")
+    })
+  })
+
+function turnToyToDiv(toyObject){
+  let toyDiv =document.createElement("div")
+  toyDiv.className = "card"
+  // toyDiv.innerText = `${toyObject}`
+  let toyHeader = document.createElement("h2")
+      toyHeader.innerText = toyObject.name
+  let toyImg = document.createElement("img")
+      toyImg.src = toyObject.image
+      toyImg.className = "toy-avatar"
+
+  let toyP = document.createElement("p")
+    toyP.innerText = toyObject.likes 
+  
+  let toyButton = document.createElement("button")
+    toyButton.className = "like-btn"
+    toyButton.innerText = "Like <3"
+  
+  toyCollection.append(toyDiv)
+  toyDiv.append(toyHeader, toyImg, toyP, toyButton)
+  console.log(turnToyToDiv)
+
+}
+
+
+
+
+
+
